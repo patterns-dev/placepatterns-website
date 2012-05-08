1efa2838c100067298e823532c274b44fc3860d8
Lev Israel <eliezer.israel@gmail.com>
2012-05-08 09:36:26 -0400

    Show Patterns and Places on home page

--- wp-content/themes/placepatterns/functions.php
+++ wp-content/themes/placepatterns/functions.php
@@ -1,7 +1,15 @@
 <?php
 add_action('init', 'types_register');
 add_action( 'wp_loaded', 'connections_register' );
- 
+add_filter( 'pre_get_posts', 'my_get_posts' );
+
+function my_get_posts( $query ) {
+	if ( is_home() && false == $query->query_vars['suppress_filters']  )
+		$query->set( 'post_type', array( 'post', 'page', 'pattern', 'place') );
+
+	return $query;
+} 
+
 function types_register() {
  
 	$pat_labels = array(
