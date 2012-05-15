139096b2e14458209bf0816213809078411f09c8
lev <lev@ip-10-99-10-31.(none)>
2012-05-15 13:12:41 -0400

    Removed date and comments from front page listing

--- wp-content/themes/placepatterns/index.php
+++ wp-content/themes/placepatterns/index.php
@@ -7,10 +7,12 @@
 	<?php if (have_posts()) : ?>
 		<div id="thumbnail">
 		<ul>
-			<?php while (have_posts()) : the_post(); ?><li id="post-<?php the_ID(); ?>" <?php post_class(); ?>><?php birdsite_the_thumbnail(); ?><h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2><p><?php the_time(get_option('date_format')); ?>
-			<?php if ('closed' <> $post->comment_status) : ?><br /><?php comments_popup_link(__('No Comments &#187;', 'birdsite'), __('1 Comment &#187;', 'birdsite'), __('% Comments &#187;', 'birdsite'), '', __('Comments Closed', 'birdsite') ); ?>
-				<?php endif; ?>
-			</p></li><?php endwhile; // no CR conform CSS ?> 
+			<?php while (have_posts()) : the_post(); ?><li id="post-<?php the_ID(); ?>" <?php post_class(); ?>><?php birdsite_the_thumbnail(); ?><h2><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+
+			<?php if(false): //LNI was: if ('closed' <> $post->comment_status) : ?>
+				<br /><?php comments_popup_link(__('No Comments &#187;', 'birdsite'), __('1 Comment &#187;', 'birdsite'), __('% Comments &#187;', 'birdsite'), '', __('Comments Closed', 'birdsite') ); ?>
+			<?php endif; ?>
+			</li><?php endwhile; // no CR conform CSS ?> 
 		</ul>
 		</div>
 
